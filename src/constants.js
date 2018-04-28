4f4d7219a52ecba4f03adaa8ca8328d680f66dd9
hailey123 <hailey.musselman@gmail.com>
2018-04-28 06:19:02 -0700

    using constants in startElectron

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
-export const DEFAULT_WORK_TIME = 3600; // 3000 seconds in 50 minutes
-export const DEFAULT_BREAK_TIME = 600; // 600 seconds in 10 minutes
+module.exports = {
+  DEFAULT_WORK_TIME: 3600, // 3000 seconds in 50 minutes
+  DEFAULT_BREAK_TIME: 600 // 600 seconds in 10 minutes
+}
--- src/startElectron.js
+++ src/startElectron.js
@@ -1,36 +1,39 @@
 // https://medium.freecodecamp.org/building-an-electron-application-with-create-react-app-97945861647c
 
-const electron = require('electron');
-const path = require('path');
-const url = require('url');
+const electron = require('electron')
+const path = require('path')
+const url = require('url')
+const constants = require('./constants')
 
 const { app, BrowserWindow, ipcMain, Tray } = electron
 
-const intervals = [15, 10]
+const intervals = [
+  constants.DEFAULT_WORK_TIME,
+  constants.DEFAULT_BREAK_TIME
+]
 let currentInterval = 0
 let interval, paused, tray
 let timeRemaining = intervals[currentInterval]
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 170,
     height: 218,
-    // resizable: false,
-    alwaysOnTop: true
-  });
+    resizable: false
+  })
 
   const startUrl = process.env.ELECTRON_START_URL || url.format({
     pathname: path.join(__dirname, '/../build/index.html'),
     protocol: 'file:',
     slashes: true
-  });
+  })
   // Load the index.html of the app.
-  mainWindow.loadURL('http://localhost:3000');
+  mainWindow.loadURL('http://localhost:3000')
 
-  // mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
       mainWindow = null
@@ -70,7 +73,7 @@ app.on('ready', () => {
   sendTime()
   interval = createInterval()
   tray = new Tray('src/assets/hourglass.png')
-});
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
