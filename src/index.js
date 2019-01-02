ff16d27ceac2c6233c3d7215ab7f4296e6585845
hailey123 <hailey.musselman@gmail.com>
2019-01-02 08:15:09 -0700

    deleted registerServiceWorker since offline app

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './react/App';
-import registerServiceWorker from './react/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
